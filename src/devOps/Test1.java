ef9cda0b6e1b15c8e09935717f0d235b3fbba7e3
skatta3 <skatta3@yahoo.com>
2023-04-02 07:04:23 +0530

    Commiting Test1.java

---
+++ src/devOps/Test1.java
@@ -0,0 +1,11 @@
+package devOps;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Test DevOps GIT Repo");
+		System.out.println("Promoting changes to dev2");
+	}
+
+}
