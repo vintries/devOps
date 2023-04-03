1237f16b9dc17fc7bac926ea0f28811345619785
vintries <vintries@yahoo.com>
2023-04-02 18:21:00 -0700

    Project converted to maven project

--- devOps2/src/main/java/com/dev/ops/data/jpa/Test.java
+++ devOps2/src/main/java/com/dev/ops/data/jpa/Test.java
@@ -1,10 +1,10 @@
-package devOps;
+package com.dev.ops.data.jpa;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Test DevOps GIT Repo");
+		System.out.println("Test....");
 	}
 
 }
