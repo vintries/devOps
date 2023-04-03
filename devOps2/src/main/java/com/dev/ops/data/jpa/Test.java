759174b672477e800a43ae5b2061456b3cb05b57
vintries <51975179+vintries@users.noreply.github.com>
2023-04-02 19:02:29 -0700

    Update Test.java
    remote file , changed

--- devOps2/src/main/java/com/dev/ops/data/jpa/Test.java
+++ devOps2/src/main/java/com/dev/ops/data/jpa/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 
 		System.out.println("can it create conflict ?, check ,,,,");
+		System.out.println("Create conflict now .....I am remote file");
 
 	}
 
